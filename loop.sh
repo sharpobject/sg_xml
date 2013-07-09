c3dd69cd019be70a69400c236ecfb77cc80ceb91
sharpobject <sharpobject@gmail.com>
2013-07-09 06:31:48 +0000

    automatic commit

--- loop.sh
+++ loop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 while true
 do
-    cd NA
-    cat urls.txt | xargs wget -N
-    cd ..
+    for dir in KR NA
+    do
+        cd $dir
+        cat urls.txt | xargs wget -N
+        cd ..
+    done
     git add */*xml
     git commit -m "automatic commit"
     git push origin master
